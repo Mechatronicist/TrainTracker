322c8970071e348e7436e9f3a92ace62bc66a67b
James Kelly <james.a.kelly.00@gmail.com>
2025-02-13 13:13:49 +1100

    Add stuff to header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import Header from "../components/Header.svelte"
+</script>
+
+<Header />
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
