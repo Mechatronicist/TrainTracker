6f03b92e3b49e5bad75509efbea70c9d1c8d2296
lewpar <lewis.o.parmenter@gmail.com>
2025-02-20 10:12:54 +1100

    Fix weather api.

--- src/routes/stop/[stop_id]/+page.server.ts
+++ src/routes/stop/[stop_id]/+page.server.ts
@@ -1,9 +1,13 @@
+import { getWeatherAsync } from "$lib/weather_api";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
     let { stop_id } = params;
     
+    let weather = await getWeatherAsync();
+
     return {
+        weather: weather,
         stopId: stop_id
     }
 }
