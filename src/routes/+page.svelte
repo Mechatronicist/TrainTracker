91efd5a2676aac88e3ec928734099f975ff280f1
lewpar <lewis.o.parmenter@gmail.com>
2025-02-20 13:31:57 +1100

    Added landing page styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,27 @@
 	export let data;
 </script>
 
-<Dropdown items={data.Stops}/>
+<div class="container">
+    <div class="dropdown-container">
+        <h1>Train Tracker</h1>
+        <div>Select a station to view time table</div>
+        <Dropdown items={data.Stops}/>
+    </div>
+</div>
+
+<style>
+.container {
+    flex: 1;
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+}
+.dropdown-container {
+    display: flex;
+    flex-direction: column;
+}
+.dropdown-container h1 {
+    text-align: center;
+}
+</style>
