6e885834a5dad224df24916754a57774df614a2c
James Kelly <james.a.kelly.00@gmail.com>
2025-02-20 12:00:05 +1100

    Added dropdown and redirect for selecting stops

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang=ts>
 	import Header from "../components/Header.svelte";
-	
 	export let data
 </script>
 
-<Header weather={data.weather}> </Header>
+<Header weather={data.Weather} stops={data.Stops}> </Header>
+
